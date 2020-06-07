94ac39a9e249932c21c2fa8093f964425149679f
myurarin <matu.urari@gmail.com>
2020-06-07 20:29:45 +0900

    みかか変換本体を作成

---
+++ __init__.py
@@ -0,0 +1,22 @@
+class MikakaConverter:
+    # みかか変換
+    def __init__(self):
+        # コンストラクタ
+        self.mikaka_list = [["Q","た"],["W","て"]]
+
+    def m_to_n(self, m_str):
+        # 平仮名から英語に変換
+        pass
+
+    def n_to_m(self, n_str):
+        # 英語から平仮名に変換
+        pass
+
+
+def main():
+    mikaka = MikakaConverter()
+    print(mikaka.mikaka_list)
+
+
+if __name__ == "__main__":
+    main()
